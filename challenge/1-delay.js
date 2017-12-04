dc5398f0c1b5ef3ada8da600a0b1736cf42a1d58
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-04 14:22:14 +1100

    Refactor solution to challenge 2

--- challenge/1-delay.js
+++ challenge/1-delay.js
@@ -2,19 +2,14 @@
 
 function delay(duration) {
   // Do magic with `new Promise` and `setTimeout`
-  return promise = new Promise((resolve, reject) => {
+  return new Promise( resolve => { // no reject because setTimeout never fails
     // We do our work, which accepts a callback function
     setTimeout(() => {
       // The function that is called after a timeout of 'duration'
       resolve()
     }, duration)
-    // }
-    // else {
-    //   reject('Error!')
-    // }
   })
 }
-
 
 function callDelay(duration) {
   console.time(`delay ${duration}ms`)
@@ -23,8 +18,19 @@ function callDelay(duration) {
   })
 }
 
+function callSyncAll(duration1, duration2, duration3) {
+  console.time(`delay callSyncAll ${duration1}ms, ${duration2}ms, ${duration3}ms`)
+  delay(duration1).then( ()=>{
+    delay(duration2).then( () => {
+      delay(duration3).then( () => {
+        console.timeEnd(`delay callSyncAll ${duration1}ms, ${duration2}ms, ${duration3}ms`)
+      })
+    })
+  })
+}
+
 function callPromiseAll(duration1, duration2, duration3) {
-  console.time(`delay Promise.all ${duration1}ms ,${duration2}ms ,${duration3}ms`)
+  console.time(`delay Promise.all ${duration1}ms, ${duration2}ms, ${duration3}ms`)
   Promise.all([
     delay(duration1),
     delay(duration2),
@@ -32,8 +38,8 @@ function callPromiseAll(duration1, duration2, duration3) {
   ])
   .then( values => {
     const [r1,r2,r3] = values
-    console.timeEnd(`delay Promise.all ${duration1}ms ,${duration2}ms ,${duration3}ms`)
+    console.timeEnd(`delay Promise.all ${duration1}ms, ${duration2}ms, ${duration3}ms`)
   })
 }
 
-module.exports = { delay, callDelay, callPromiseAll }
+module.exports = { callDelay, callSyncAll, callPromiseAll }
--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const Path = require('path')
 // const oneTxtPath = Path.join(__dirname, 'data', '1.txt')
 // writeTimestamp(oneTxtPath)
 
-const { callDelay, callPromiseAll } = require('./challenge/1-delay')
+const { callDelay, callSyncAll, callPromiseAll } = require('./challenge/1-delay')
 
 // Challenge 1
 callDelay(2000)
 
 // Challenge 2
-callDelay(400)
-callDelay(500)
-callDelay(700)
+callSyncAll(400, 500, 700)
 
 // Challenge 3
 callPromiseAll(400, 500, 700)
